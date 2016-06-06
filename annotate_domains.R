21382e2e4f40fe6f14d4419d9716f42be0766be6
Hiren Joshi <hirenj@gmail.com>
2016-06-06 16:05:47 +0200

    Finish mapping for human domains

--- annotate_domains.R
+++ annotate_domains.R
@@ -13,10 +13,52 @@ if ( ! exists('interpro_raw_go')) {
 go_manual_mappings = read.delim('go_manual.tsv')
 interpro_manual_mappings = read.delim('interpro_manual.tsv')
 
+if ( ! exists('all_sites.human')) {
+  devtools::source_gist('b79cb9f63bf6d7c6cb68')
+  gator.flatpack <- as.data.frame(gator.flatpack)
+  msdatas = do.call(rbind,lapply(grep("msdata",ls(envir = Rgator:::getDataEnvironment()),value=T), function(set) {
+    datas = get(set)
+    datas = datas[datas$multiple.protein == FALSE & grepl("[STY]",datas$peptide),]
+    hexnac_ratios = Rgator:::hexnac.ratios(datas)
+    if (! is.null(hexnac_ratios) && nrow(hexnac_ratios) > 0) {
+      hexnac_ratios = lapply(split(hexnac_ratios$ratio,hexnac_ratios$spectra),function(x) median(x,na.rm=T) )
+      datas = datas[datas$spectra %in% names(Filter(function(median) { median <= 1.1 },hexnac_ratios)),]
+    }
+    if (nrow(datas) < 1) {
+      return(datas)
+    }
+    datas$biosrc = gsub(".msdata.json","",set,fixed=T)
+    datas$source.free = datas$source
+    datas$compartment[grepl("tcl",datas$source.free,ignore.case=T)] <- 'tcl'
+    datas$compartment[grepl("sec",datas$source.free,ignore.case=T)] <- 'sec'
+    datas[,c('uniprot','site','peptide.start','peptide.end','biosrc','source.free','compartment')]
+  }))
+
+  gator.flatpack = gator.flatpack[! gator.flatpack$biosrc %in% c('fibroblast_hcmv','fibroblast_hcmv2','fibroblast_vzv','fibroblast_vzv2','hacat_hsv2'),]
+  gator.flatpack = rbind(gator.flatpack,setNames(msdatas,names(gator.flatpack)))
+
+  gator.flatpack$site = ifelse(is.na(gator.flatpack$sites.site),floor((as.numeric(gator.flatpack$peptide.start)+as.numeric(gator.flatpack$peptide.end))/2),gator.flatpack$sites.site )
+  all_sites = unique(gator.flatpack[!is.na(gator.flatpack$site),c('uniprot','site')])
+  all_sites.human = all_sites[all_sites$uniprot %in% tolower(Rgator::getUniprotIds(9606)),]
+}
+
+if ( is.null(get('transmembrane.uniprot.9606')) ) {
+  Rgator:::downloadTransmembrane(9606)
+}
+if ( is.null(get('interpro.9606')) ) {
+  Rgator::downloadInterproDomains(9606)
+}
+
+human.domains = rbind( interpro.9606.annotated[interpro.9606.annotated$class == 'Domain',c('uniprot','dom','start','end')], transmembrane.uniprot.9606 )
+
+
 get_classes = function(interpro) {
   go_mapped = merge(interpro_raw_go[interpro_raw_go$interpro %in% interpro,], go_manual_mappings,by='go')[,c('interpro','Class')]
   manual_mapped = interpro_manual_mappings[interpro_manual_mappings$interpro %in% interpro,c('interpro','Class')]
   all_mapped = rbind(go_mapped,manual_mapped)
+  if (length(unique(all_mapped$interpro)) == length(unique(interpro))) {
+    return (all_mapped)
+  }
   return (rbind(all_mapped, data.frame(interpro=interpro[!interpro %in% all_mapped$interpro],Class=NA)))
 }
 
@@ -72,7 +114,6 @@ add_child_counts = function(term_counts,groups) {
 
 overlapping_domains = function(query_interpro,target_interpro,all_domains) {
   domains = all_domains[all_domains$dom %in% c(query_interpro,target_interpro),]
-  message(nrow(domains))
   plyr::ddply(domains,c('uniprot'),function(doms) {
     if ( length(intersect(doms$dom,query_interpro)) < 1 ) {
       return ()
@@ -121,18 +162,18 @@ apply_classes = function(domains,classes) {
   return (domains)
 }
 
-#all_groups = group_interpros(interpro_raw_relations)
+all_groups = group_interpros(interpro_raw_relations)
 
 if ( ! exists('domainsets')) {
   domainsets = Rgator::calculateDomainSets(all_sites.human,'site',human.domains,stem_distance=50,remove_tm_overlaps = TRUE)
 }
 
-#raw_counts = table(unique(rbind(domainsets$inside[,c('uniprot','dom')],domainsets$between[,c('uniprot','dom')]))$dom)
-#child_summed_counts = sort(add_child_counts(raw_counts,all_groups))
+raw_counts = table(unique(rbind(domainsets$inside[,c('uniprot','dom')],domainsets$between[,c('uniprot','dom')]))$dom)
+child_summed_counts = sort(add_child_counts(raw_counts,all_groups))
 
-#high_count_terms = child_summed_counts[child_summed_counts >= 10]
+high_count_terms = child_summed_counts[child_summed_counts >= 10]
 
-#low_count_terms = child_summed_counts[child_summed_counts < 10 & ! names(child_summed_counts) %in% unlist(sapply(names(high_count_terms),function(x) child_terms(x,all_groups,10))) ]
+low_count_terms = child_summed_counts[child_summed_counts < 10 & ! names(child_summed_counts) %in% unlist(sapply(names(high_count_terms),function(x) child_terms(x,all_groups,10))) ]
 
 all_classes = get_classes(c(names(high_count_terms),names(low_count_terms[low_count_terms >= 2])))
 
@@ -143,6 +184,8 @@ overlap_counts = table(unique(overlaps)$key)
 overlap_mapping = unique(overlaps[ ! grepl("TMhelix",overlaps$key) & overlaps$key %in% names(overlap_counts[overlap_counts > 2]),c('query','target')])
 
 overlap_classes = setNames(merge(overlap_mapping,all_classes,by.x='target',by.y='interpro')[,c('query','Class')],c('interpro','Class'))
+
+all_classes = get_classes(unique(domainsets$all$dom))
 
 all_classes = rbind(all_classes[! is.na(all_classes$Class),],overlap_classes)
 
@@ -156,8 +199,8 @@ final_classes = expand_classes(all_classes)
 
 human.glycodomains = apply_classes(human.domains,final_classes)
 
-cytosolic = read.delim('cytosolic.txt',header=F)$V1
+#cytosolic = read.delim('cytosolic.txt',header=F)$V1
 
-domainsets.glycodomain = Rgator::calculateDomainSets(all_sites.human[! all_sites.human$uniprot %in% cytosolic,],'site',human.glycodomains,stem_distance=50,remove_tm_overlaps = FALSE)
+domainsets.glycodomain = Rgator::calculateDomainSets(all_sites.human,'site',human.glycodomains,stem_distance=50,remove_tm_overlaps = TRUE)
 
-sort(table(unique(domainsets.glycodomain$inside[,c('uniprot','dom')])$dom))
+#sort(table(unique(domainsets.glycodomain$inside[,c('uniprot','dom')])$dom))
